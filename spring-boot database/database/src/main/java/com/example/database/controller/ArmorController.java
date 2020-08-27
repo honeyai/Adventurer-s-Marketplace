04bdfd42b8575cbfd87edcabbdbdb43c7104311f
Jessica <contact.jessicasanchez@gmail.com>
2020-08-27 18:35:34 -0400

    starting armor controller

--- spring-boot database/database/src/main/java/com/example/database/controller/ArmorController.java
+++ spring-boot database/database/src/main/java/com/example/database/controller/ArmorController.java
@@ -1,4 +1,20 @@
 package com.example.database.controller;
 
+import com.example.database.models.Armor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/armor")
 public class ArmorController {
+
+  //@ Autowired
+  //ArmorService armorService;
+
+  @GetMapping("/all")
+  public Iterable<Armor> findAll() {
+    return null;
+  }
 }
