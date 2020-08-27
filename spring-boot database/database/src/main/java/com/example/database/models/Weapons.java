5f85de409c90abffb935f818e4334ee6d6665d4b
Jessica <contact.jessicasanchez@gmail.com>
2020-08-27 18:32:30 -0400

    Created weapons model

--- spring-boot database/database/src/main/java/com/example/database/models/Weapons.java
+++ spring-boot database/database/src/main/java/com/example/database/models/Weapons.java
@@ -1,4 +1,71 @@
 package com.example.database.models;
 
+import org.w3c.dom.Text;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "weapons")
 public class Weapons {
+
+  @Id
+  @Column
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  private Long id;
+
+  @Column
+  private String nameOfItem;
+
+  @Column
+  private String type;
+
+  @Column
+  private String price;
+
+  @Column
+  private Text description;
+
+  public Weapons() {
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNameOfItem() {
+    return nameOfItem;
+  }
+
+  public void setNameOfItem(String nameOfItem) {
+    this.nameOfItem = nameOfItem;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public void setType(String type) {
+    this.type = type;
+  }
+
+  public String getPrice() {
+    return price;
+  }
+
+  public void setPrice(String price) {
+    this.price = price;
+  }
+
+  public Text getDescription() {
+    return description;
+  }
+
+  public void setDescription(Text description) {
+    this.description = description;
+  }
 }
