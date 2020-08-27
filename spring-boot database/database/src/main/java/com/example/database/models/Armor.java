f0479d59d1208b8c1aa02de3f427abf24f95562e
Jessica <contact.jessicasanchez@gmail.com>
2020-08-27 18:23:17 -0400

    Created armor model

--- spring-boot database/database/src/main/java/com/example/database/models/Armor.java
+++ spring-boot database/database/src/main/java/com/example/database/models/Armor.java
@@ -1,4 +1,70 @@
 package com.example.database.models;
 
+import org.w3c.dom.Text;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name= "Armor")
 public class Armor {
+
+  @Id
+  @Column
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+  private Long id;
+
+  @Column
+  private String nameOfItem;
+
+  @Column
+  private String AC;
+
+  @Column
+  private String Price;
+
+  @Column
+  private Text Description;
+
+  public Armor() {}
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getNameOfItem() {
+    return nameOfItem;
+  }
+
+  public void setNameOfItem(String nameOfItem) {
+    this.nameOfItem = nameOfItem;
+  }
+
+  public String getAC() {
+    return AC;
+  }
+
+  public void setAC(String AC) {
+    this.AC = AC;
+  }
+
+  public String getPrice() {
+    return Price;
+  }
+
+  public void setPrice(String price) {
+    Price = price;
+  }
+
+  public Text getDescription() {
+    return Description;
+  }
+
+  public void setDescription(Text description) {
+    Description = description;
+  }
 }
