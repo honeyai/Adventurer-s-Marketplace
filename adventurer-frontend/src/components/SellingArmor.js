164170a959c96fb5eda5d6c7db710c3f53dc016b
Jessica <contact.jessicasanchez@gmail.com>
2020-08-28 18:18:33 -0400

    trying to request data and render to screen

--- adventurer-frontend/src/components/SellingArmor.js
+++ adventurer-frontend/src/components/SellingArmor.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import './Styles/sellingArmor.css'
 // import { Card } from "@material-ui/core";
 
 const SellingArmor = () => {
@@ -10,48 +11,69 @@ const SellingArmor = () => {
     ac: "",
   });
 
-  const handleChange = event => {
-    const {value, id } = event.target;
-    setInput({ ...input, [id]: value});
-  }
+  const [items, setItems] = useState(null);
 
-  const handleSubmit = async event => {
+  const handleChange = (event) => {
+    const { value, id } = event.target;
+    setInput({ ...input, [id]: value });
+  };
+
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    try{
-      const response = await axios.post('http://localhost:8080/armor/sellArmor', input);
-
-      console.log(
-        "this is what the response.data is: ",
-        response.data
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/armor/sellArmor",
+        input
       );
-    } catch (error){
+
+      console.log("this is what the response.data is: ", response.data);
+    } catch (error) {
       console.error(error.message);
     }
   };
 
+  const getWares = async () => {
+    try {
+      let response = await axios.get("http://localhost:8080/armor/list")
+      console.log(response.data);
+    } catch(error) {
+      console.error("Error has occurred!", error.message)
+    }
+  } 
+
   return (
-    <div>
-      you're at the selling armor page
-      <form onChange={ event => handleChange(event)} onSubmit={ event => handleSubmit(event)}>
-        <label>
-          Item Name:
-          <input type="text" id="nameOfItem"></input>
-        </label>
-        <label>
-          Item AC:
-          <input type="text" id="ac"></input>
-        </label>
-        <label>
-          Item Price:
-          <input type="text" id="price"></input>
-        </label>
-        <label>
-          Description:
-          <input type="text" id="description"></input>
-        </label>
-        
-        <input type="submit" />
-      </form>
+    <div className="sellingArmor__wrapper">
+      <div className="sellingArmor__FormContainerPage">
+        <form
+          className="sellingArmor__Form"
+          onChange={(event) => handleChange(event)}
+          onSubmit={(event) => handleSubmit(event)}
+        >
+          <label>
+            Item Name:
+            <input type="text" id="nameOfItem"></input>
+          </label>
+          <label>
+            Item AC:
+            <input type="text" id="ac"></input>
+          </label>
+          <label>
+            Item Price:
+            <input type="text" id="price"></input>
+          </label>
+          <label>
+            Description:
+            <input type="text" id="description"></input>
+          </label>
+
+          <input type="submit" />
+        </form>
+      </div>
+      <div className="sellingArmor__sellItems">
+        {
+          items ?  <div> Items will show </div> : <div> You aren't selling any wares yet! </div>
+        }
+      </div>
     </div>
   );
 };
