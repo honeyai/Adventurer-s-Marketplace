bf3ef234b3fa8b8e7d6fc31f2f0a0f4db06ef8fb
Jessica <contact.jessicasanchez@gmail.com>
2020-08-28 14:30:29 -0400

    Added link routes

--- adventurer-frontend/src/components/SellersPage.js
+++ adventurer-frontend/src/components/SellersPage.js
@@ -3,35 +3,55 @@ import { MDBTypography } from "mdbreact";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import "./Styles/sellersPage.css";
+import { CardContent, Typography } from "@material-ui/core";
+import { Link } from "react-router-dom";
 const SellersPage = () => {
   return (
     <div>
       <MDBTypography tag="h1">What are you selling</MDBTypography>
       <div className="sellersPage__cardContainer">
-        <Card className="sellersPage__card">
-          <CardMedia
-            id="cardMedia"
-            image={require("./Assets/Images/armor.jpg")}
-            title="Armor"
-          />
-          hshfsh
-        </Card>
-
-        <Card className="sellersPage__card">
-        <CardMedia
-            id="cardMedia"
-            image={require("./Assets/Images/weapons.jpg")}
-            title="Armor"
-          />
-        </Card>
-
-        <Card className="sellersPage__card">
-        <CardMedia
-            id="cardMedia"
-            image={require("./Assets/Images/potions.jpg")}
-            title="Armor"
-          />
-        </Card>
+        <Link to="/armor">
+          <Card className="sellersPage__card">
+            <CardMedia
+              id="cardMedia"
+              image={require("./Assets/Images/armor.jpg")}
+              title="Armor"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Armor
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link to="/weapons">
+          <Card className="sellersPage__card">
+            <CardMedia
+              id="cardMedia"
+              image={require("./Assets/Images/weapons.jpg")}
+              title="Weapons"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Weapons
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
+        <Link to="/potions">
+          <Card className="sellersPage__card">
+            <CardMedia
+              id="cardMedia"
+              image={require("./Assets/Images/potions.jpg")}
+              title="Potions"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="h2">
+                Potions
+              </Typography>
+            </CardContent>
+          </Card>
+        </Link>
       </div>
     </div>
   );
